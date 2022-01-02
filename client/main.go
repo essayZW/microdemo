9ce656151806d06d643f15a65af826ff466ec403
essay <essaybeihai@gmail.com>
2022-01-02 19:18:41 +0800

    init client

---
+++ client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/asim/go-micro/plugins/registry/etcd/v4"
+	pb "github.com/essayZW/microdemo/content/proto"
+	"go-micro.dev/v4"
+	"go-micro.dev/v4/registry"
+)
+
+func main() {
+	etcdRegistry := etcd.NewRegistry(
+		registry.Addrs("172.17.0.2:2379"),
+		etcd.Auth("root", "beihai"),
+	)
+
+	src := micro.NewService(
+		micro.Registry(etcdRegistry),
+	)
+
+	client := pb.NewContentService("content", src.Client())
+
+	resp, err := client.Query(context.TODO(), &pb.ContentId{
+		Id: 1,
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println(resp)
+}
